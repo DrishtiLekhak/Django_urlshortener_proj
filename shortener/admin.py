4adf60126f9573a17c23ae14a11505e05d163f30
DrishtiLekhak <deepalekhak49@gmail.com>
2026-06-09 18:29:06 +0545

    Admin customization

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ShortURL
+
+@admin.register(ShortURL)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = ('short_code', 'original_url','click_count', 'created_at')
+    search_fields = ('short_code', 'original_url','click_count')
+    ordering = ['id']
